453a4e966a2b1fa28090d282e36fbd6105a53017
Roman Hrushchak <roman.hrushchak@ut.ee>
2016-12-06 01:20:37 +0200

    added the old website from dropbox

---
+++ js/utils.js
@@ -0,0 +1,12 @@
+function refreshAbout() {
+	$("#content").load("about.html");
+}
+function refreshEax() {
+	$("#content").load("eax.html");
+}
+function refreshDownload() {
+	$("#content").load("download.html");
+}
+function refreshNews() {
+	$("#content").load("news.html");
+}
